73ca451decf716e489349a7210aca9b7079abf80
FieldMarshalGaig <mooresoscar1@gmail.com>
2022-11-12 13:23:06 +0000

    stuff2

---
+++ src/game/Card.java
@@ -0,0 +1,14 @@
+public class Card {
+    
+    private int cardValue;
+
+    public void setCardValue(int value) {
+        this.cardValue = value;
+    }
+
+    public int getCardValue() {
+        return this.cardValue;
+    }
+
+
+}
